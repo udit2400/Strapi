cfb5a3694ba6a1d3079a535edd0ed15ac45f7238
Udit Kumawat <73550469+udit2400@users.noreply.github.com>
2024-09-26 18:21:36 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,101 +1,71 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import React, { useEffect, useState } from 'react'
+
+const page = () => {
+  
+  const [data,setData]=useState([]);
+  const [item,setItem]=useState({
+    Title:'Industry Experts',
+    Description:'Industry exports refer to goods and services that are produced in one country and sold to other countries.',
+    
+  });
+  const [select, setSelect] = useState(1);
+  
+
+  useEffect(()=>{
+    const fetchData=async()=>{
+      try {
+        const response=await fetch('https://bright-ants-d6d02dc50f.strapiapp.com/api/dummies?populate=*');
+        const result=await response.json();
+        console.log('result', result);
+        setData(result.data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchData();
+  },[])
+
+  const handleClick=(id)=>{
+      const item=data.find(item=>item.id===id);
+      setItem(item.attributes);
+      setSelect(id);
+      console.log('clicked- ', item.attributes);
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className='flex w-[100vw] h-[100vh] flex-col items-center gap-5 mt-5 text-2xl '>
+      <h1>WHY CHOOSE US</h1> 
+      <h1 className='font-bold'>We Are Different From Others</h1>   
+      <h1 className='mx-32 text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, perferendis obcaecati. Lorem ipsum dolor sit amet. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci, eum?</h1>  
+      <div className='flex mt-6 '>
+        <div className='relative flex h-[55vh] w-[40vw]'>
+            <div className='absolute h-full flex flex-col w-[25vw] z-10 rounded-full text-white bg-red-600 bg-opacity-70 justify-center text-lg gap-6 px-10'>
+              <h1 className='font-semibold'>{item.Title}</h1>
+              <p>{item.Description}</p>
+            </div>
+            <div className='absolute h-full right-0 flex w-[25vw] rounded-full text-white justify-center text-lg'>
+              <img className='w-full h-full rounded-full object-cover' src={item.Image? `http://localhost:1337${item.Image.data.attributes.url}`:'https://media.istockphoto.com/id/613241758/photo/young-woman-working-on-a-laptop.jpg?s=612x612&w=0&k=20&c=B3WbJ7VFEr77G0T698I0WMkM5LOV-pMrLCljEEJGh-o='} alt="" />
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className='h-[55vh] w-[40vw]'>
+          <div className='flex flex-col items-end mr-10 text-lg font-semibold '>
+            {
+             data && data.map((item) =>{
+                return (
+                  <div key={item.id} onClick={()=>{handleClick(item.id)}} className={`flex my-3 rounded-l-3xl justify-between pl-5  w-96 py-2 pr-5 ${select==item.id?'bg-red-500 text-white':'bg-slate-200'} `}>
+                    <h1>&lt;</h1>
+                    <h1>{item.attributes.Title}</h1>
+                  </div>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
+
+export default page
